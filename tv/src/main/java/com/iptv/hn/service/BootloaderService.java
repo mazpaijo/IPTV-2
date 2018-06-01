27cbaabf109fb76e60c7fe3afbe9936e3b8487d3
hs <362481334@qq.com>
2018-06-01 15:36:37 +0800

    测试1

--- tv/src/main/java/com/iptv/hn/service/BootloaderService.java
+++ tv/src/main/java/com/iptv/hn/service/BootloaderService.java
@@ -1038,8 +1038,7 @@ public class BootloaderService extends IntentService {
             @Override
             public void onFinish(Object... o) {
 //                int keyCode = (int)o[0];
-
-                Log.d("myShow", "onFinish: ");
+                Log.d("myShow", "onFinish: 点击按钮完毕～ ");
                 hideAdsDialog(context, adsBean);
                 //行为日志上报
                 String user = Utils.getTvUserId(context);
